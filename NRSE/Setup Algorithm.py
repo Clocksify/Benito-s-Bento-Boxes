93c0641b1f16e704b4aa2544ee6c98a0ff17845e
EltonNotFound <167483376+EltonNotFound@users.noreply.github.com>
2024-05-05 12:09:02 +0800

    Add files via upload

--- NRSE/Setup Algorithm.py
+++ NRSE/Setup Algorithm.py
@@ -5,7 +5,7 @@ import json
 from Crypto.Util.Padding import unpad
 from Crypto.Util.Padding import pad
 
-with open("my key.txt", "r") as secrets_file:
+with open("Benito-s-Bento-Boxes/my key.txt", "r") as secrets_file:
     key = secrets_file.readline().strip().encode('utf-8')
 
     if len(key) < 32:
@@ -15,7 +15,8 @@ with open("my key.txt", "r") as secrets_file:
         hash_key = key [:32]
 
 iv = Random.get_random_bytes(10)
-
+with open("Benito-s-Bento-Boxes/NRSE/iv.txt",'wb') as f:
+    f.write(iv)
 e_cipher = AES.new(hash_key, AES.MODE_CTR, nonce = iv)
 d_cipher = AES.new(hash_key, AES.MODE_CTR, nonce = iv)
 
@@ -24,7 +25,7 @@ EQ_to_EQR_dict = {}
 for i in range(1, 4):
     hash = SHA256.new(data = bytes(str(i), 'utf-8'))
     hash_attr = hash.hexdigest()
-    with open(f"NRSE/file{i}.txt", "r") as name:
+    with open(f"Benito-s-Bento-Boxes/NRSE/file{i}.txt", "r") as name:
         name = name.readline().strip().encode('utf-8')
         ciphertext = e_cipher.encrypt(name)
         print (ciphertext)
@@ -50,7 +51,7 @@ for key, value in EQ_to_EQR_dict.items():
     EQ_to_EQR_dict_final[str_key] = str_list
 
 print(EQ_to_EQR_dict_final)
-with open("NRSE/EQ_to_EQR_dict.json", "w") as EQ_to_EQR_dict_dest:
+with open("Benito-s-Bento-Boxes/NRSE/EQ_to_EQR_dict.json", "w") as EQ_to_EQR_dict_dest:
     EQ_to_EQR_dict_final = EQ_to_EQR_dict_dest.write(json.dumps(EQ_to_EQR_dict_final))
 #Expected output is a json file EQ_to_EQR_dict.json where there are 3 separate 32 byte hashes that are attr1,2 and 3 respectively
 #However, no files are in the json file's dictionary output
