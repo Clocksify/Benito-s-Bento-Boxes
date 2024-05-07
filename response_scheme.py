517aa0daef1f7e7d989c01f74e91eadbb88389eb
EltonNotFound <167483376+EltonNotFound@users.noreply.github.com>
2024-05-07 21:17:02 +0800

    Add files via upload

---
+++ response_scheme.py
@@ -0,0 +1,18 @@
+import json
+
+with open('Benito-s-Bento-Boxes/NRSE/EQ_to_EQR_dict.json','r') as f:
+    dic = json.load(f)
+
+open('Benito-s-Bento-Boxes/NRSE/Response.txt',"w").close()
+
+with open("Benito-s-Bento-Boxes/NRSE/Encrypted_query.txt", "r") as j:
+    Encrypted_query = j.readlines()
+    print(Encrypted_query)
+    for line in Encrypted_query:
+        For_real = line.strip()
+        Response = dic[For_real]
+        for lines in Response:
+            Response1 = lines.strip()
+            open('Benito-s-Bento-Boxes/NRSE/Response.txt',"a").write((Response1))
+            open('Benito-s-Bento-Boxes/NRSE/Response.txt',"a").write('\n')
+
