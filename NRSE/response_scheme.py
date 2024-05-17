95f0f54bb856120f719f919507187ba433ed89bb
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-05-17 20:41:13 +0800

    Update response_scheme.py

--- NRSE/response_scheme.py
+++ NRSE/response_scheme.py
@@ -7,7 +7,6 @@ open('Benito-s-Bento-Boxes/NRSE/Response.txt',"w").close()
 
 with open("Benito-s-Bento-Boxes/NRSE/Encrypted_query.txt", "r") as j:
     Encrypted_query = j.readlines()
-    print(Encrypted_query)
     for line in Encrypted_query:
         encrypted_attr = line.strip()
         Response = dic[encrypted_attr]
