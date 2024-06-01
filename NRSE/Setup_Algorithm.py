ebce17410dba77fafa3670d8c8727a9358c0f290
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-06-01 13:10:45 +0800

    Update Setup_Algorithm.py

--- NRSE/Setup_Algorithm.py
+++ NRSE/Setup_Algorithm.py
@@ -5,7 +5,7 @@ import json
 from Crypto.Util.Padding import unpad
 from Crypto.Util.Padding import pad
 
-with open("Benito-s-Bento-Boxes/NRSE/my_key.txt", "r") as secrets_file:
+with open("my_key.txt", "r") as secrets_file:
     key = secrets_file.readline().strip().encode('utf-8')
 
     if len(key) < 32:
@@ -14,20 +14,22 @@ with open("Benito-s-Bento-Boxes/NRSE/my_key.txt", "r") as secrets_file:
     if len(key) > 32:
         hash_key = key [:32]
 
-iv = Random.get_random_bytes(10)
-with open("Benito-s-Bento-Boxes/NRSE/iv.txt",'wb') as f:
-    f.write(iv)
-e_cipher = AES.new(hash_key, AES.MODE_CTR, nonce = iv)
-d_cipher = AES.new(hash_key, AES.MODE_CTR, nonce = iv)
+# iv = Random.get_random_bytes(10)
+# with open("Benito-s-Bento-Boxes/NRSE/iv.txt",'wb') as f:
+#     f.write()
 
 EQ_to_EQR_dict = {}
+iv_dict = {}
 
 for i in range(1, 4):
     hash = SHA256.new(data = bytes(str(i), 'utf-8'))
     hash_attr = hash.hexdigest()
-    with open(f"Benito-s-Bento-Boxes/NRSE/file{i}.txt", "r") as name:
+    with open(f"file{i}.txt", "r") as name:
         name = name.readline().strip().encode('utf-8')
-        ciphertext = e_cipher.encrypt(name)
+        e_cipher = AES.new(hash_key, AES.MODE_CBC)
+        ciphertext = e_cipher.encrypt(pad(name, AES.block_size))
+        iv = e_cipher.iv
+        iv_dict[i] = iv
         EQ_to_EQR_dict[hash_attr] = [ciphertext]
 
             # change to if hash_atr in file_bytes with proper use case
@@ -49,7 +51,7 @@ for key, value in EQ_to_EQR_dict.items():
     EQ_to_EQR_dict_final[str_key] = str_list
 
 print(EQ_to_EQR_dict_final)
-with open("Benito-s-Bento-Boxes/NRSE/EQ_to_EQR_dict.json", "w") as EQ_to_EQR_dict_dest:
+with open("EQ_to_EQR_dict.json", "w") as EQ_to_EQR_dict_dest:
     EQ_to_EQR_dict_final = EQ_to_EQR_dict_dest.write(json.dumps(EQ_to_EQR_dict_final))
 #Expected output is a json file EQ_to_EQR_dict.json where there are 3 separate 32 byte hashes that are attr1,2 and 3 respectively
 #However, no files are in the json file's dictionary output
