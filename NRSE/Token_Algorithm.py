0ef53aecd20fafc32b4e4a9937634f4ead3f3335
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-06-01 13:11:56 +0800

    Update Token_Algorithm.py

--- NRSE/Token_Algorithm.py
+++ NRSE/Token_Algorithm.py
@@ -1,6 +1,6 @@
 from Crypto.Hash import SHA256
 
-with open("Benito-s-Bento-Boxes/NRSE/my_key.txt", "r") as secrets_file:
+with open("my_key.txt", "r") as secrets_file:
     key = secrets_file.readline().strip().encode('utf-8')
 
     if len(key) < 32:
@@ -11,14 +11,14 @@ with open("Benito-s-Bento-Boxes/NRSE/my_key.txt", "r") as secrets_file:
 
 Encrypted_query = []
 
-start_int = int(input("What is your starting range? "))
+start_int = int(input("What is your starting range? ")) 
 end_int = int(input("What is your ending range? "))
 
-for j in range(1, 4):
+for j in range(start_int, end_int):
         hash = SHA256.new(data = bytes(str(j), 'utf-8'))
         Encrypted_query_item = hash.hexdigest()
         Encrypted_query.append(Encrypted_query_item + '\n')
 
-with open("Benito-s-Bento-Boxes/NRSE/Encrypted_query.txt", "w") as Encrypted_query_dest:
+with open("Encrypted_query.txt", "w") as Encrypted_query_dest:
         Encrypted_query_dest.writelines(Encrypted_query)
 # with open("Benito-s-Bento-Boxes/NRSE/Encrypted_query.txt", "w") as EQ_to_EQR_dict_dest:
