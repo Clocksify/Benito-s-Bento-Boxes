34f1412ff654970e5ea6c439e74955b0563741a7
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-06-01 13:12:52 +0800

    Update Setup_Algorithm.py

--- NRSE/Setup_Algorithm.py
+++ NRSE/Setup_Algorithm.py
@@ -5,7 +5,7 @@ import json
 from Crypto.Util.Padding import unpad
 from Crypto.Util.Padding import pad
 
-with open("my_key.txt", "r") as secrets_file:
+with open("Benito-s-Bento-Boxes/NRSE/my_key.txt", "r") as secrets_file:
     key = secrets_file.readline().strip().encode('utf-8')
 
     if len(key) < 32:
@@ -24,7 +24,7 @@ iv_dict = {}
 for i in range(1, 4):
     hash = SHA256.new(data = bytes(str(i), 'utf-8'))
     hash_attr = hash.hexdigest()
-    with open(f"file{i}.txt", "r") as name:
+    with open(f"Benito-s-Bento-Boxes/NRSE/file{i}.txt", "r") as name:
         name = name.readline().strip().encode('utf-8')
         e_cipher = AES.new(hash_key, AES.MODE_CBC)
         ciphertext = e_cipher.encrypt(pad(name, AES.block_size))
@@ -51,7 +51,7 @@ for key, value in EQ_to_EQR_dict.items():
     EQ_to_EQR_dict_final[str_key] = str_list
 
 print(EQ_to_EQR_dict_final)
-with open("EQ_to_EQR_dict.json", "w") as EQ_to_EQR_dict_dest:
+with open("Benito-s-Bento-Boxes/NRSE/EQ_to_EQR_dict.json", "w") as EQ_to_EQR_dict_dest:
     EQ_to_EQR_dict_final = EQ_to_EQR_dict_dest.write(json.dumps(EQ_to_EQR_dict_final))
 #Expected output is a json file EQ_to_EQR_dict.json where there are 3 separate 32 byte hashes that are attr1,2 and 3 respectively
 #However, no files are in the json file's dictionary output
