44f07a3ef2671c0061c0bb5018c4ac29ce485201
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-09-02 09:50:19 +0800

    Create LOG Setup.py

---
+++ LOG Setup.py
@@ -0,0 +1,19 @@
+H = []
+EDS = {}
+def node(start, end):
+  if start = end:
+    H += (start, start)
+  mid = (start + end) // 2
+  left_child = node(start, mid)
+  right_child = node(mid+1, end)
+  H += left_child
+  H += right_child
+
+node(1, LOG.max)
+for node in H:
+  for v in range(1, LOG.max):
+    CF += 
+    EDS[node] = CF
+  
+  
+     
