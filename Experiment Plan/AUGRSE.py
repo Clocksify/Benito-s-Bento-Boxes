2a282b987d81c9ab4e29430cea5269aa58afeef8
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-11-17 19:56:15 +0800

    Update AUGRSE.py

--- Experiment Plan/AUGRSE.py
+++ Experiment Plan/AUGRSE.py
@@ -1,11 +1,32 @@
-RSEmax = 8
+
 import math
 
-for i in range(1, math.log(8, 2)+1):
-  level0 = ((1, RSEmax))
-  for node in f"level{i-1}":
-    f"level{i}" = set()
-    mid = (node[0] + node[1]) // 2
-    f"level{i}".add((node[0], mid))
-    f"level{i}".add(((node[0]+mid)//2+1, (mid+1+node[1])//2))
-    f"level{i}".add((mid, node[1]))
+def cover_query(start, end, node_start, node_end):
+    result= []
+
+    levels = {}
+    levels[0]= {(1,node_end)}
+    for i in range(1, int(math.log(node_end, 2))+1):
+        levels[i] = set()
+        for node in levels[i-1]: #levels[0]: (1,8) levels[1]:{...} 
+            mid = (node[0] + node[1]) // 2
+            levels[i].add((node[0], mid))
+            levels[i].add(((node[0]+mid)//2+1, (mid+1+node[1])//2))
+            levels[i].add((mid+1, node[1]))
+
+    level_dict = dict(sorted(levels.items()))
+    level_dict = {key: sorted(value) for key, value in level_dict.items()}
+    
+    for level, nodes in level_dict.items():
+        for node in list(nodes):
+            for n in result:
+                condition = node[0] <= n[1] and node[0] <= n[1]
+                if start <= node[0] and node[0] <= end and not condition:
+                    result.append(node)
+                    break
+        continue
+    
+    return result
+
+LOGmax = 8
+print(cover_query(2, 6, 1, LOGmax))
