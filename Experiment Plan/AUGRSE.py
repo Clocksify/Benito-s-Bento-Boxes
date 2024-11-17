a899ccb7a655abc5380199cf4aaf5ab4680e9b23
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-11-17 20:15:24 +0800

    Update AUGRSE.py

--- Experiment Plan/AUGRSE.py
+++ Experiment Plan/AUGRSE.py
@@ -1,19 +1,23 @@
 
 import math
 
-def cover_query(start, end, node_start, node_end):
-    result= []
+result= []
+levels = {}
+levels[0]= {(1,node_end)}
+for i in range(1, int(math.log(RSEmax, 2))+1):
+    levels[i] = set()
+    for node in levels[i-1]: #levels[0]: (1,8) levels[1]:{...} 
+        mid = (node[0] + node[1]) // 2
+        levels[i].add((node[0], mid))
+        levels[i].add(((node[0]+mid)//2+1, (mid+1+node[1])//2))
+        levels[i].add((mid+1, node[1]))
 
-    levels = {}
-    levels[0]= {(1,node_end)}
-    for i in range(1, int(math.log(node_end, 2))+1):
-        levels[i] = set()
-        for node in levels[i-1]: #levels[0]: (1,8) levels[1]:{...} 
-            mid = (node[0] + node[1]) // 2
-            levels[i].add((node[0], mid))
-            levels[i].add(((node[0]+mid)//2+1, (mid+1+node[1])//2))
-            levels[i].add((mid+1, node[1]))
-
+def cover(a, b):
+    for i in range(1, int(math.log(RSEmax, 2))+1):
+        for node in levels[i]:
+            if (node[0] >= a) and (node[1] <= b):
+                if 
+    
     level_dict = dict(sorted(levels.items()))
     level_dict = {key: sorted(value) for key, value in level_dict.items()}
     
