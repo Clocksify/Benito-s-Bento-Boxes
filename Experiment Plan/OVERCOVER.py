d0a63f31390b9abeca2e00d17f912cd6270f787f
wilsonthehecker <153520347+Wilsonthelistener@users.noreply.github.com>
2024-11-21 19:22:15 +0800

    Create OVERCOVER.py

---
+++ Experiment Plan/OVERCOVER.py
@@ -0,0 +1,24 @@
+import math
+
+RSEmax = 16
+
+def is_power_of_two(x):
+    return (x & (x - 1)) == 0
+
+def overcover(start, end):
+    diff = (end - start) - 1
+    while True:
+        diff += 1
+        if is_power_of_two(diff + 1):  # Check if diff + 1 is a power of 2
+            N = math.log(diff + 1, 2)
+            break
+    if start == end:
+        return (start, end)
+    else:
+        for i in range(int(N), int(math.log(RSEmax, 2)) + 1):  # Ensure N is an integer
+            for j in range(0, start):
+                if (start - j) % 2**i == 1:
+                    if start - j + 2 ** i - 1 >= end:
+                        return (start - j, start - j + 2 ** i - 1)
+                    else:
+                        break
